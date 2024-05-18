c824e01def7acd79f86398118b6928809b449cd0
Simon Rabuogi <symokevo@gmail.com>
2024-05-18 17:37:51 +0300

    Set up Model Associations and Validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,14 @@
 class Company < ApplicationRecord
+  has_many :users
+  before_create :generate_company_id
+
+  validates :name, presence: true, uniqueness: true
+
+  private
+  def generate_company_id
+    loop do
+      self.company_id = "#{name[0].upcase}#{SecureRandom.alphanumeric(7).upcase}"
+      break unles Company.exists?(company_id: company_id)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :company
+  belongs_to :company, optional: true
+
+  validates :full_name, :role, :email, presence: true
+  validates :email, uniqueness: true
 end
