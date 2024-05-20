4a0b9211b668a9eea2affbfaa1ccfc26c318dbf2
Simon Rabuogi <symokevo@gmail.com>
2024-05-20 04:34:11 +0300

    Create and configure the necesary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'company_heads/index'
-  get 'company_heads/approve'
-  get 'company_heads/destroy'
-  get 'users/new'
-  get 'users/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :companies do
+    resources :company_heads, only: [:index] do
+      member do
+        patch :approve
+        delete :destroy
+      end
+    end
+  end
 
-  # Defines the root path route ("/")
-  root "users#home"
-  resources :users
+  root "users#new"
 end
