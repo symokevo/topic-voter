933b5ea80ab03a4a2247b15a7e1406cf4d2e4849
Simon Rabuogi <symokevo@gmail.com>
2024-05-26 15:02:42 +0300

    create Navbar and add links

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,11 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "popper"
 import "bootstrap"
+
+// app/javascript/packs/application.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarHeight = document.querySelector(".navbar.fixed-top").offsetHeight;
+    document.body.style.paddingTop = `${navbarHeight}px`;
+  });
+  
